4a83952f2bedc307a373e0a76a1e4a7a75859599
Alejandro Sánchez <elcone@gmail.com>
2026-09-10 09:02:21 -0700

    feature: separate bank commissions from regular expenses

--- models/silver/gastos.sql
+++ models/silver/gastos.sql
@@ -2,9 +2,14 @@ SELECT
     last_day(movimiento.fecha) AS periodo,
     movimiento.recibo,
     movimiento.importe,
-    coalesce(gasto.categoria, 'No especificado') AS tipo_gasto,
+    CASE
+        WHEN movimiento.es_comision_bancaria = TRUE THEN 'Comisiones bancarias'
+        ELSE coalesce(gasto.categoria, 'No especificado')
+    END AS tipo_gasto,
     gasto.concepto,
     movimiento.descripcion AS concepto_banco
 FROM silver.bajio_movimientos AS movimiento
-LEFT JOIN bronze.gsheets_gastos AS gasto ON movimiento.recibo = gasto.recibo_bancario
+LEFT JOIN bronze.gsheets_gastos AS gasto
+    ON movimiento.recibo = gasto.recibo_bancario
+    AND movimiento.es_comision_bancaria = FALSE
 WHERE movimiento.tipo = 'EGRESO'
